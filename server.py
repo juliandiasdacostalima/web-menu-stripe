3be798390dbf05fedd94c3dde5ed489dbb00b73a
juliandiasdacostalima <103572225+juliandiasdacostalima@users.noreply.github.com>
2025-04-22 17:40:30 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,57 @@
+import os
+from flask import Flask, redirect, request, render_template
+import stripe
+from dotenv import load_dotenv
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+stripe.api_key = "sk_test_51RFa3uAc0zWUIiwIyOky03kYSRsQZLrkEzE4EAJ1FbzjDnDo9vgSPWOSbi4fdN3sV3MlwpNdHCZSUzcBgFLhmdHU00nYCPN6RQ"
+
+app = Flask(__name__)
+
+products = [
+    {'id': 'prod_1', 'name': 'Producto 1', 'price': 10},
+    {'id': 'prod_2', 'name': 'Producto 2', 'price': 15},
+    {'id': 'prod_3', 'name': 'Producto 3', 'price': 20}
+]
+
+YOUR_DOMAIN = 'http://localhost:4242'
+
+@app.route('/')
+def index():
+    return render_template('index.html', products=products)
+
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+    try:
+        selected_products = request.form.getlist('product')
+        line_items = []
+
+        for product_id in selected_products:
+            product = next((p for p in products if p['id'] == product_id), None)
+            if product:
+                line_items.append({
+                    'price_data': {
+                        'currency': 'eur',
+                        'product_data': {
+                            'name': product['name'],
+                        },
+                        'unit_amount': product['price'] * 100,
+                    },
+                    'quantity': 1
+                })
+
+        checkout_session = stripe.checkout.Session.create(
+            line_items=line_items,
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/success.html',
+            cancel_url=YOUR_DOMAIN + '/cancel.html',
+        )
+    except Exception as e:
+        return str(e)
+
+    return redirect(checkout_session.url, code=303)
+
+if __name__ == '__main__':
+    app.run(port=4242)
